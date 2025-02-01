de84a535c97ceacd52a71ce7d7590a3b0929c854
Shiberger <hannarong.tonkra@gmail.com>
2025-02-02 03:26:22 +0700

    Initial Commit

---
+++ HearingTestApp/HearingTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearingTestAppApp.swift
+//  HearingTestApp
+//
+//  Created by Hannarong Kaewkiriya on 2/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct HearingTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
